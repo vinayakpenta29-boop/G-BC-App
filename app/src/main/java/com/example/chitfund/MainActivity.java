9c7e19b1461959cc2675c7ccf3fcf3b7d47e1296
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-07-06 01:13:28 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/chitfund/MainActivity.java
+++ app/src/main/java/com/example/chitfund/MainActivity.java
@@ -513,7 +513,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         TableRow footerRow = new TableRow(this);
-        footerRow.setBackgroundColor(Color.parseColor("#EFE6DB"));
+        footerRow.setBackgroundColor(Color.parseColor("#E6DEC8"));
         footerRow.setPadding(4, 12, 4, 12);
 
         TextView tvTotalLbl = new TextView(this); tvTotalLbl.setText("GRAND TOTALS"); tvTotalLbl.setPadding(20, 12, 20, 12); tvTotalLbl.setTextColor(Color.parseColor("#0F172A")); tvTotalLbl.setTypeface(null, Typeface.BOLD); footerRow.addView(tvTotalLbl);
